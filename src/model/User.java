152bc51db58bd50bb9e9d92b970fd755cda26594
2542390738@qq.com <2542390738@qq.com>
2020-01-10 09:59:45 +0800

    4

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,67 @@
 package model;
 
 public class User {
-	
+	private int id;
+	private String name;
+	private String account;
+	private String password;
+	private String phone;
+	private String sex;
+	private String place;
+	private int type;
+	private int state;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
 }
