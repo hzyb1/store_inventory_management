b5256393534abf76dc26aa45a2ef1f135be7c61c
2542390738@qq.com <2542390738@qq.com>
2020-01-11 11:39:10 +0800

    11one

--- src/serviceImpl/SupplierServiceImpl.java
+++ src/serviceImpl/SupplierServiceImpl.java
@@ -1,7 +1,82 @@
 package serviceImpl;
 
+import org.apache.ibatis.session.SqlSession;
+
+import mapper.SupplierMapper;
+import mapper.UserMapper;
+import model.Supplier;
 import service.SupplierService;
+import tools.DBTools;
 
 public class SupplierServiceImpl implements SupplierService{
+  SupplierMapper supplierMapper;
+  SqlSession sqlSession;
+  
+public Boolean updateSupplier(Supplier supplier)
+{ 
+		
+		try {
+			sqlSession=DBTools.getSession();
+			SupplierMapper supplierMapper =sqlSession.getMapper(SupplierMapper.class);
+			boolean state=supplierMapper.updateSupplier(supplier);
+			sqlSession.commit();
+			return state;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			sqlSession.rollback();
+			return null;
+		}finally{
+		sqlSession.close();
+	     }
 	
+    }
+
+
+public Supplier selectSupplierById(int id) {
+	try{
+		sqlSession=DBTools.getSession(); 
+		SupplierMapper supplierMapper = sqlSession.getMapper(SupplierMapper.class);
+		Supplier supplier = supplierMapper.selectById(id);
+		return supplier;
+	}catch(Exception e){
+		e.printStackTrace();
+		sqlSession.rollback();
+		return null;
+	}finally{
+		sqlSession.close();
+	}
+}
+
+public Boolean insertSupplier(Supplier supplier) {
+	try{
+		sqlSession=DBTools.getSession(); 
+		SupplierMapper supplierMapper = sqlSession.getMapper(SupplierMapper.class);
+		boolean state = supplierMapper.insertSupplier(supplier);
+		sqlSession.commit();
+		return state;
+	}catch(Exception e){
+		e.printStackTrace();
+		sqlSession.rollback();
+		return null;
+	}finally{
+		sqlSession.close();
+	}
+}
+public boolean deleteSupplier(int id) {
+	try{
+		sqlSession=DBTools.getSession(); 
+		SupplierMapper supplierMapper = sqlSession.getMapper(SupplierMapper.class);
+		boolean state = supplierMapper.deleteById(id);
+		sqlSession.commit();
+		return state;
+	}catch(Exception e){
+		e.printStackTrace();
+		sqlSession.rollback();
+	}finally{
+		sqlSession.close();
+	}
+	return false;
+}
+
 }
--- src/serviceImpl/Test.java
+++ src/serviceImpl/Test.java
@@ -2,13 +2,29 @@ package serviceImpl;
 
 import java.util.List;
 
+import model.Supplier;
 import model.User;
 
-public class Test {
-	public static void main(String[] args) {
-		UserServiceImpl userServiceImpl = new UserServiceImpl();
-		User user = userServiceImpl.selectUserById(3);
-		user.setName("风日文");
-		userServiceImpl.deleteUser(19);
-	}
+//public class Test {
+//	public static void main(String[] args) {
+//		UserServiceImpl userServiceImpl = new UserServiceImpl();
+//		User user = userServiceImpl.selectUserById(2);
+//		user.setName("日文");
+//		userServiceImpl.addUser(user);
+//	}
+//}
+public class Test{
+	
+	public static void main(String[] args)
+	{
+		SupplierServiceImpl supplierServiceImpl =new SupplierServiceImpl();
+		Supplier supplier=supplierServiceImpl.selectSupplierById(1);
+		
+		supplier.setName("儿六");
+		supplier.setAddress("上海");
+//		supplier.setId(2);
+		supplier.setPhone("123456");
+		supplierServiceImpl.insertSupplier(supplier);
+		System.out.println(supplier);
+}
 }
