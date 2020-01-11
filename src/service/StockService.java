e4bf166d7688bbd7bb260abb1bb8a499aa836c79
2542390738@qq.com <2542390738@qq.com>
2020-01-11 17:25:32 +0800

    1123

--- src/service/StockService.java
+++ src/service/StockService.java
@@ -1,5 +1,12 @@
 package service;
 
-public interface StockService {
+import java.util.List;
 
+import model.Stock;
+
+public interface StockService {
+	public List<Stock> selectAllStock();
+	public Boolean updateStock(Stock stock);
+	public Boolean insertStock(Stock stock);
+	public boolean deleteStock(int id);
 }
--- src/serviceImpl/StockServiceImpl.java
+++ src/serviceImpl/StockServiceImpl.java
@@ -1,7 +1,63 @@
 package serviceImpl;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+
+import mapper.StockMappper;
+import mapper.SupplierMapper;
+import model.Stock;
 import service.StockService;
+import tools.DBTools;
 
 public class StockServiceImpl implements StockService{
+  SqlSession sqlSession;
+  StockMappper stockMapper;
+	@Override
+	public List<Stock> selectAllStock() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-}
+	@Override
+	public Boolean updateStock(Stock stock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean insertStock(Stock stock) {
+		try{
+			sqlSession=DBTools.getSession(); 
+			StockMappper stockMapper = sqlSession.getMapper(StockMappper.class);
+			boolean state = stockMapper.insertStock(stock);
+			sqlSession.commit();
+			return state;
+		}catch(Exception e){
+			e.printStackTrace();
+			sqlSession.rollback();
+			return null;
+		}finally{
+			sqlSession.close();
+		}
+	}
+
+	@Override
+	public boolean deleteStock(int id) {
+		try{
+			sqlSession=DBTools.getSession(); 
+			StockMappper stockMapper = sqlSession.getMapper(StockMappper.class);
+			boolean state = stockMapper.deleteById(id);
+			sqlSession.commit();
+			return state;
+		}catch(Exception e){
+			e.printStackTrace();
+			sqlSession.rollback();
+		}finally{
+			sqlSession.close();
+		}
+		return false;
+	}
+
+	}
+
